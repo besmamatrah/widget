a0970e7b6b0a392f8f2e22b6369122075dc5cae7
besmamatrah <159618274+besmamatrah@users.noreply.github.com>
2024-02-12 19:15:55 +0100

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,19 +1,14 @@
-(function (){
+(function () {
     var chatbotUrl = 'https://chatbot-2w3r.onrender.com/';
 
-    function loadChatbot(){
+    function loadChatbot() {
         var iframe = document.createElement('iframe');
         iframe.src = chatbotUrl;
-        iframe.styleSheets.width = '300px';
+        iframe.style.width = '300px'; // Corrected property name
         iframe.style.height = '500px';
         document.body.appendChild(iframe);
     }
 
+    // Ensure the chatbot loads when the window is fully loaded
     window.onload = loadChatbot;
 })();
-
-
-/*
-how to include in the actual website 
-    <script src="the link"></script>
-*/
